ec99f02f73811c5d688b8a4b8e47c3cf053d0b7a
JensGeorg <33146097+JensGeorg@users.noreply.github.com>
2022-08-05 15:00:53 +0200

    Create plot_segment.r

---
+++ plot_segment.r
@@ -0,0 +1,22 @@
+plot_segment<-function(segments){
+  o<-segments
+  plot(o$position,o$delay,ylim=c(0, max(o$delay)),pch=19)
+  segs<-unique(o$delay_fragment)
+  oo<-grep("O",segs)
+  if(length(oo)>0){
+    segs<-segs[-oo]
+  }
+  for(j in 1:length(segs)){
+    temp<-which(o$delay_fragment==segs[j])
+    temp<-o[temp,]
+    intercept<-temp$intercept[1]
+    slope<-temp$slope[1]
+    s<-min(temp$position)
+    e<-max(temp$position)
+    curve(slope*x+intercept, add=T, from=s, to=e, lwd=2)
+	mid<-s+(e-s)/2
+	y_mid<-slope*mid+intercept+0.5
+	velocity<-round(1/slope/60,1)
+	text(mid, y_mid, labels=paste0(velocity, " nt/s"))
+  }    
+}
