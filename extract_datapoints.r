7fc085dbbdae7e9154668038c403e27ba02c5c4b
JensGeorg <33146097+JensGeorg@users.noreply.github.com>
2022-08-05 13:57:20 +0200

    Update extract_datapoints.r

--- extract_datapoints.r
+++ extract_datapoints.r
@@ -5,23 +5,38 @@
 #timesteps: total steps of the simulation
 #time_points: sample timepoints in seconds
 
-extract_datapoints<-function(probe_all, rif_time,timesteps, time_points=c(1,c(1,2,3,4,6,8,10,15,20)*60)){
-	data_new<-list()
-	for(i in 1:length(probe_all[[1]])){
-		Data5<-c()
+
+extract_datapoints<-function(probe_all, rif_time,timesteps, time_points=c(0,1,2,3,4,6,8,10,15,20)*60, reps=1){
+	if(reps==1){
+		data_new<-c()
 		for(j in 1:length(probe_all)){
-			 temp<-probe_all[[j]][[i]]
-			 temp<-temp[rif_time:timesteps]
-			 tim<-time_points
-			 tmp<-temp[tim]
-			 position<-rep(as.numeric(names(probe_all[[1]])[1]),length(tmp))
-			 group<-rep(1,length(tmp))
-			 out<-cbind((tim-1)/60,tmp,group, position)
-			 colnames(out)<-c("time","inty","group","position")
-			 Data5<-rbind(Data5,out)
-			  
+			temp<-probe_all[[j]]
+			temp<-temp[rif_time:timesteps]
+			tmp<-temp[time_points+1]
+			position<-rep(as.numeric(names(probe_all)[j]),length(tmp))
+			out<-cbind((time_points)/60,tmp, position)
+			colnames(out)<-c("time","inty","position")
+			data_new<-rbind(data_new,out)
 		}
-		data_new[[i]]<-as.data.frame(Data5)
+		data_new<-as.data.frame(data_new)
+	} else{
+		data_new<-list()
+		for(i in 1:length(probe_all[[1]])){
+			Data5<-c()
+			for(j in 1:length(probe_all)){
+				 temp<-probe_all[[j]][[i]]
+				 temp<-temp[rif_time:timesteps]
+				 tmp<-temp[time_points+1]
+				 position<-rep(as.numeric(names(probe_all[[1]])[1]),length(tmp))
+				 out<-cbind((time_points)/60,tmp, position)
+				 colnames(out)<-c("time","inty","position")
+				 Data5<-rbind(Data5,out)
+				  
+			}
+			data_new[[i]]<-as.data.frame(Data5)
+		}
 	}
 	data_new
 }
+
+
